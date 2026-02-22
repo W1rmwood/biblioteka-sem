b4dbf21c79bd296a245805f0ded1e01fa7444f8e
minsk <minskiyilja@gmail.com>
2026-02-22 13:59:22 +0300

    first class1

--- include/Class1.h
+++ include/Class1.h
@@ -1,2 +1,19 @@
 #pragma once
 
+#include "Class2.h"
+#include <vector>
+
+
+class Library {
+    private:
+        std::vector <Book> books;
+    
+    public:
+        Library();
+
+        int booksCount();
+
+        void printBook();
+
+        Book getBookByIndex(int index);
+};
